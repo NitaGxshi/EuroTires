3570c4bc650783620d242662d91a359a9847729b
NitaGxshi <120093333+NitaGxshi@users.noreply.github.com>
2024-01-08 13:28:12 +0100

    creating routes.js

---
+++ backend/routes/routes.js
@@ -0,0 +1,130 @@
+// import express 
+import express from "express";
+// import functions from controller 
+import {
+    showtires,
+    showtireById,
+    createtire,
+    updatetire,
+    deletetire,
+} from "../controllers/tire.js";
+
+import {
+    showAUser,
+    createAccount
+} from "../controllers/user.js";
+
+import {
+    addItems,
+    getItem,
+    updateItem,
+    allItems,
+    deleteItem,
+    deleteItems
+} from "../controllers/cart.js";
+
+import {
+    createBooking
+} from "../controllers/booksession.js";
+
+import {
+    createBillDetails,getBillDetailsById
+} from "../controllers/billdetails.js";
+
+import {
+    showNewestStatusId,
+    createBillStatus, 
+    getAllBillsByUser,
+    getAllBillsByBill,
+    getAllBills,
+    updateBillStatus,
+    updateBillPaid,
+    cancelBillStatus
+} from "../controllers/billstatus.js";
+
+// init express router
+const router = express.Router();
+
+////////////////////////// tire ////////////////////////////////
+// get all tire
+router.get("/api/tires", showtires);
+
+// get single tire 
+router.get("/api/tires/:id", showtireById);
+
+// create tire
+router.post("/api/tires", createtire);
+
+// update tire 
+router.put("/api/tires/:id", updatetire);
+
+// delete tire
+router.delete("/api/tires/:id", deletetire);
+
+
+
+////////////////////////// USER ////////////////////////////////
+// get all user
+router.get("/api/users/:email", showAUser);
+
+// create account
+router.post("/api/users/", createAccount);
+
+
+
+////////////////////////// CART ////////////////////////////////
+// add to cart
+router.post("/api/cartItem", addItems);
+
+// get a item in cart
+router.get("/api/cartItem/:user_id/:tire_id", getItem);
+
+// get all items by user id
+router.get("/api/cartItem/:id", allItems);
+
+// update item qty
+router.put("/api/cartItem/", updateItem);
+
+// delete a item in cart
+router.delete("/api/cartItem/:user_id/:tire_id", deleteItem);
+
+// delete all items in cart
+router.delete("/api/cartItem/:id", deleteItems);
+
+
+
+////////////////////////// Booking ////////////////////////////////
+router.post("/api/booking", createBooking);
+
+
+
+////////////////////////// Bill Details ////////////////////////////////
+router.post("/api/billdetails", createBillDetails);
+router.get("/api/billdetails/:id", getBillDetailsById);
+
+
+
+////////////////////////// Bill Status ////////////////////////////////
+router.get("/api/billstatus/new", showNewestStatusId);
+router.post("/api/billstatus", createBillStatus);
+router.get("/api/billstatus/user/:id", getAllBillsByUser);
+router.get("/api/billstatus/bill/:id", getAllBillsByBill);
+router.get("/api/billstatus", getAllBills);
+router.put("/api/billstatus/:id", updateBillStatus);
+router.put("/api/billstatus/paid/:id", updateBillPaid);
+router.put("/api/billstatus/cancel/:id", cancelBillStatus);
+
+
+
+
+
+// export default router
+export default router;
+
+
+
+
+
+
+
+
